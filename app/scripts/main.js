1678462c1a8facc1d63da07239aede15ad65d769
Alvaro José Solis <alvarojose827@gmail.com>
2016-11-03 14:42:54 -0500

    3.2 : Parte 1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,31 +24,32 @@ angular.module('cityApp', ['cityApp.controllers', 'cityApp.factories', 'ngRoute'
 	by just injecting the Factory name into the controller dependencies.
 */
 angular.module('cityApp.factories', [])
-.factory('cityFactory', function(){
-	/* In this case, we are going to store our cities list into a variable within the Factory */
+.factory('cityFactory', function($http){
+	//-- In this case, we are going to store our cities list into a variable within the Factory 
+	
+	/*
 	var cities = [
-		{'name': 'Amsterdam', 'country': 'Netherlands'},
-		{'name': 'Eindhoven', 'country': 'Netherlands'},
-		{'name': 'The Haage', 'country': 'Netherlands'},
-		{'name': 'Brussels', 'country': 'Belgium'},
-		{'name': 'Brugge', 'country': 'Belgium'},
-		{'name': 'Hamburg', 'country': 'Germany'},
-		{'name': 'Düsseldorf', 'country': 'Germany'},
-		{'name': 'Köln', 'country': 'Germany'},
-		{'name': 'Venice', 'country': 'Italy'},
-		{'name': 'Firenze', 'country': 'Italy'},
-		{'name': 'Milano', 'country': 'Italy'},
-		{'name': 'Paris', 'country': 'France'},
-		{'name': 'San Francisco', 'country': 'US'},
-		{'name': 'Dallas', 'country': 'US'},
-		{'name': 'Miami', 'country': 'US'},
-		{'name': 'Los Angeles', 'country': 'US'},
-		{'name': 'New York', 'country': 'US'},
-		{'name': 'Brisbane', 'country': 'Australia'},
-		{'name': 'Sydney', 'country': 'Australia'},
-		{'name': 'Melbourne', 'country': 'Australia'},
-		{'name': 'Gold Coast', 'country': 'Australia'}
+		{"name": "Amsterdam", "country": "Netherlands"},
+		{"name": "Eindhoven", "country": "Netherlands"},
+		{"name": "The Haage", "country": "Netherlands"},
+		{"name": "Brussels", "country": "Belgium"},
+		{"name": "Brugge", "country": "Belgium"},
+		{"name": "Hamburg", "country": "Germany"},
+		{"name": "Düsseldorf", "country": "Germany"},
+		{"name": "Köln", "country": "Germany"},
+		{"name": "Venice", "country": "Italy"},
+		{"name": "Firenze", "country": "Italy"},
+		{"name": "Milano", "country": "Italy"},
+		{"name": "Paris", "country": "France"},
+		{"name": "San Francisco", "country": "US"},
+		{"name": "Dallas", "country": "US"},
+		{"name": "Miami", "country": "US"},
+		{"name": "Los Angeles", "country": "US"},
+		{"name": "New York", "country": "US"},
+		{"name": "Brisbane", "country": "Australia"},
+		{"name": "Sydney", "country": "Australia"},
 	];
+	*/
 
 	/*Every Factory needs to return an object with the methods/functions that are going to be accesible from the controller where the Factory is injected.*/
 	return {
@@ -56,6 +57,7 @@ angular.module('cityApp.factories', [])
 		getAll: function(){
 			return cities;
 		},
+
 		/*AddCity will receive 2 parameters and add a new city-country into the array*/
 		addCity: function(_city, _country){
 			cities.push({'name': _city, 'country': _country});
@@ -66,10 +68,13 @@ angular.module('cityApp.factories', [])
 
 angular.module('cityApp.controllers', [])
 //-- Notice that 'cityFactory' is being injected as dependency
-.controller('MainController', ['$scope', 'cityFactory', function($scope, cityFactory){
+.controller('MainController', ['$scope', 'cityFactory', '$http', function($scope, cityFactory, $http){
 	//-- Now we can just equal $scope.cities to what is stored in the Factory.
-	$scope.cities = cityFactory.getAll();
+	//$scope.cities = cityFactory.getAll();
 
+	$http.get('json/cities.json').success(function(data) {
+		$scope.cities = data;
+	});
 }])
 
 //-- Notice that 'cityFactory' is being injected as dependency
